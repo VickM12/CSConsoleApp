5a18f0d19ffe936ecebab5449e2de0a3ed48c9b6
Vick Moberg <vicktor.moberg@gmail.com>
2022-08-20 17:18:34 -0400

    added transactions class and some withdrawals

--- Program.cs
+++ Program.cs
@@ -9,6 +9,22 @@ namespace MySuperBank
         {
             var account = new BankAccount("Vicktor", 1000);
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+
+            try
+{
+    var invalidAccount = new BankAccount("invalid", -55);
+}
+catch (ArgumentOutOfRangeException e)
+{
+    Console.WriteLine("Exception caught creating account with negative balance");
+    Console.WriteLine(e.ToString());
+    return;
+}
         }
     }
 }
